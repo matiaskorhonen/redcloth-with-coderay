b870e2e7c0caa65b6d26d2f02443913902b8c99a
Matias Korhonen <webadmin@draco-vulgaris.net>
2009-09-08 16:39:14 +0300

    Fixed a problem caused by the merge

--- lib/redclothcoderay.rb
+++ lib/redclothcoderay.rb
@@ -8,9 +8,9 @@ module RedclothCoderay
     text.gsub(SOURCE_TAG_REGEXP) do |m|
       all_of_it = $~[1]
       whitespace_before = $~[2]
-      lang = ($~[4] || :ruby).to_sym
-      code = $~[5].strip
-      whitespace_after = $~[6]
+      lang = ($~[3] || :ruby).to_sym
+      code = $~[4].strip
+      whitespace_after = $~[5]
       
       wrap_in = all_of_it =~ /\n/ ? MULTI_LINE : SINGLE_LINE
       highlighted = wrap_in % CodeRay.scan(code, lang).div(CODERAY_OPTIONS)
